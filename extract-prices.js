7efcfcca2f426eeffc65d85ecfe4e4f1c87728d0
jsasek83 <jsasek83@gmail.com>
2017-01-02 14:53:25 -0800

    Adding an amazon scraper

--- extract-prices.js
+++ extract-prices.js
@@ -1,32 +1,48 @@
 var ebayscraper = require('./lib/scrapers/ebay-price-scraper');
+var amazonscraper = require('./lib/scrapers/amazon-price-scraper');
 var httpGetPromise = require('./lib/helpers/httpGetPromise');
-var util = require('util');
 
 sites = [
   {name:'Ebay',
-   host:'http://www.ebay.com',
+   protocol : 'http',
+   host:'www.ebay.com',
    queryPattern:'/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.X.TRS2&_nkw=%s&_sacat=0',
-   priceExtracter : ebayscraper
+   priceExtracter : ebayscraper,
+   enabled : false
+ },
+ {name:'Amazon',
+  protocol : 'https',
+  host:'www.amazon.com',
+  queryPattern:'/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s',
+  priceExtracter : amazonscraper,
+  enabled : true,
+  headers : {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding':'utf-8',
+    'Accept-Language':'en-US,en;q=0.8,en-GB;q=0.6,fr;q=0.4',
+    'Host':'www.amazon.com',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
   }
+}
 ]
 
 function queryForPrices(options, query) {
 
-      var formattedQuery = encodeURIComponent(query);
-      console.log('Formatted Query: ' + formattedQuery);
-
       var extractPrice = (element) => {
-
-        var queryPath = util.format(element.queryPattern, formattedQuery);
-
-        console.log('Querying: ' + element.host + ' Path: ' + queryPath);
 
         return new Promise((resolve,reject) => {
 
-          httpGetPromise(element.host + queryPath)
+          if(!element.enabled){
+            console.log('Site is not enabled: ' + element.name);
+            resolve();
+            return;
+          }
+
+          httpGetPromise(element, query)
           .then((html) => {
               console.log('Got Results For: ' + element.host + ' Size: ' + html.length);
-              return element.priceExtracter(html);
+              return element.priceExtracter(html, query, element);
           })
           .then((productJson) => {
               console.log('Found the following information: ' + JSON.stringify(productJson));
@@ -45,5 +61,5 @@ function queryForPrices(options, query) {
 }
 
 console.log('Starting Test Run of Extract Prices');
-queryForPrices(null, 'Wilson Jones Heavy-Duty Round Ring View Binder, 1 inch, White, 4ct')
+queryForPrices(null, 'Dyson Pure Hot+Cool Purifier Heater')
   .then(function(){console.log('Finishing Run of Extract Prices')});
